7aac94e80e55750f0411992df41382cd85b2b093
Perry Valdez <perry.valdez@ugroop.com.au>
2017-04-14 14:40:42 +0800

    Added sample tests via Mocha

---
+++ tests/test_parselines.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var assert = require('assert');
+var easymetrologic = require('../libs/easymetrologic.js');
+
+describe('Mocha test', () => {
+	it('Mocha should be running', () => {
+		assert(true);
+	});
+	
+});
