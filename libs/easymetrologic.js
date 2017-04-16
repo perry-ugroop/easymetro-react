fc518218a18b9489bf7e907a0508caeffe932fe5
Perry Valdez <perry.valdez@ugroop.com.au>
2017-04-16 09:14:03 +0800

    Added more tests

--- libs/easymetrologic.js
+++ libs/easymetrologic.js
@@ -3,10 +3,15 @@
 class LineNetwork {
 	constructor() {
 		this._lines = [];
+		this._stations = [];
+	}
+	
+	_cloneObj(s) {
+		return JSON.parse(JSON.stringify(s));
 	}
 	
 	addLine(lineName, arrStationNames) {
-		let lineObj = { 'name': lineName, 'stations': []};
+		let lineObj = { 'name': lineName, 'stations': arrStationNames};
 		this._lines.push(lineObj);
 	}
 	
@@ -19,7 +24,8 @@ class LineNetwork {
 	}
 	
 	getLineStationNames(index) {
-		
+		let stns = this._lines[index].stations;
+		return this._cloneObj(stns);
 	}
 }
 
@@ -32,14 +38,25 @@ module.exports = {
 			
 			if(str) {
 				let lineName = str;
+				let stations = [];
 				
 				let colonPos = str.indexOf(':');
 				if(colonPos > -1) {
 				    lineName = str.substr(0, colonPos);
+					let stationStr = str.substr(colonPos + 1).trim();
+					let splitted = stationStr.split(/\s*,\s*/);
+					
+					for(let i in splitted) {
+						let stn = splitted[i].trim();
+						
+						if(stn) {
+						    stations.push(stn);
+						}
+					}
 				}
 				
 				retVal = new LineNetwork;
-				retVal.addLine(lineName, ['a', 'b', 'c', 'd']);
+				retVal.addLine(lineName, stations);
 				
 			    return retVal;	
 			}
