f0288b4bf0bd901a0623ee154c45f8ff3ed0857f
Perry Valdez <perry.valdez@ugroop.com.au>
2017-04-16 08:39:52 +0800

    Added tests

--- libs/easymetrologic.js
+++ libs/easymetrologic.js
@@ -1,8 +1,53 @@
 'use strict';
 
-module.exports = {
+class LineNetwork {
+	constructor() {
+		this._lines = [];
+	}
+	
+	addLine(lineName, arrStationNames) {
+		let lineObj = { 'name': lineName, 'stations': []};
+		this._lines.push(lineObj);
+	}
+	
+	getLineCount() {
+		return this._lines.length;
+	}
+	
+	getLineName(index) {
+		return this._lines[index].name;
+	}
+	
+	getLineStationNames(index) {
+		
+	}
+}
+
+module.exports = {	
 	parseLinesSpec: function(str) {
-		alert('Hello, World: ' + str);
+		let retVal = {};
+		
+		if(str) {
+			str = str.trim();
+			
+			if(str) {
+				let lineName = str;
+				
+				let colonPos = str.indexOf(':');
+				if(colonPos > -1) {
+				    lineName = str.substr(0, colonPos);
+				}
+				
+				retVal = new LineNetwork;
+				retVal.addLine(lineName, ['a', 'b', 'c', 'd']);
+				
+			    return retVal;	
+			}
+			
+			
+		}
+		
+		return retVal;
 	},
 	
 };
--- tests/test_parselines.js
+++ tests/test_parselines.js
@@ -3,9 +3,45 @@
 var assert = require('assert');
 var easymetrologic = require('../libs/easymetrologic.js');
 
-describe('Mocha test', () => {
-	it('Mocha should be running', () => {
-		assert(true);
+describe('Test parsing line specs', () => {
+	it('Parsing a null should yield an empty object', () => {
+		let result = easymetrologic.parseLinesSpec(null);
+		assert(JSON.stringify(result) === JSON.stringify({}));
 	});
+	
+	it('Parsing an empty string should yield an empty object', () => {
+		let result = easymetrologic.parseLinesSpec('');
+		assert(JSON.stringify(result) === JSON.stringify({}));
+	});
+
+	it('Parsing a series of whitespaces should yield an empty object', () => {
+		let result = easymetrologic.parseLinesSpec('    ');
+		assert(JSON.stringify(result) === JSON.stringify({}));
+	});
+
+	it('Parsing a series of empty lines should yield an empty object', () => {
+		let result = easymetrologic.parseLinesSpec('\n\n\n\n');
+		assert(JSON.stringify(result) === JSON.stringify({}));
+	});
+
+	describe('Parsing the string "LineName: Station1, Station2, Station3" should yield a valid line network object', () => {
+		let result = easymetrologic.parseLinesSpec('LineName: Station1, Station2, Station3');
+		
+        it('Line count should be 1', () => {		    
+		    assert(result.getLineCount() === 1);
+		});
+		
+        it('First line should be named "LineName"', () => {
+            let index = 0;		    
+		    assert(result.getLineName(index) === 'LineName');
+		});
+		
+        it('Stations of the first line should be Station1, Station2 and Station3', () => {
+            let index = 0;
+            let stations = result.getLineStationNames(index);
+            stations.sort();			
+		    assert(JSON.stringify(stations) === JSON.stringify(['Station1', 'Station2', 'Station3']));
+		});
+	});	
 	
 });
