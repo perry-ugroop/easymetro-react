6692dbc76204fa3a81928d3991a9c19fb9f2e9ab
Perry Valdez <perry.valdez@ugroop.com.au>
2017-04-25 14:37:42 +1000

    Added diagram

--- tests/test_traversals.js
+++ tests/test_traversals.js
@@ -4,6 +4,21 @@ var assert = require('assert');
 var easymetrologic = require('../libs/easymetrologic.js');
 var utils = require('../libs/utils.js');
 
+/***
+    Sample line network:
+
+                   Line B
+ 
+                     B1
+                     B2    C9
+Line A      A1 A2 A3 AB A5 AC A7
+                     B4    C7
+                     B5    C6
+Line C         C1 C2 BC C4 C5
+                     B7
+
+***/
+
 var network = `A: A1, A2, A3, AB, A5, AC, A7
 B: B1, B2, AB, B4, B5, BC, B7
 C: C1, C2, BC, C4, C5, C6, C7, AC, C9`;
